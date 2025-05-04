52b81e77006fc4f4a07b59f2fd1c6af272a813cb
elluhhh <160988909+elluhhh@users.noreply.github.com>
2025-05-05 01:50:06 +1000

    created project

---
+++ CinemaApp/CinemaApp/CinemaAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CinemaAppApp.swift
+//  CinemaApp
+//
+//  Created by Ella Gibbs on 5/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct CinemaAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CinemaApp/CinemaApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CinemaApp
+//
+//  Created by Ella Gibbs on 5/5/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
